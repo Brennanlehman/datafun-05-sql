d4b6d9267ab6a90b3ca12f73a616b84a564980c3
Brennan Lehman <blehman@nwmissouri.edu>
2024-02-06 11:00:37 -0600

    updates

--- brennan_sql.py
+++ brennan_sql.py
@@ -3,7 +3,7 @@ import pandas as pd
 import pathlib
 
 # Define the database file in the current root project directory
-db_file = pathlib.Path("project.sqlite3")
+db_file = pathlib.Path("project.db")
 
 def create_database():
     """Function to create a database. Connecting for the first time
@@ -17,8 +17,48 @@ def create_database():
     except sqlite3.Error as e:
         print("Error creating the database:", e)
 
+def create_tables():
+    """Function to read and execute SQL statements to create tables"""
+    try:
+        with sqlite3.connect(db_file) as conn:
+            sql_file = pathlib.Path("sql_file", "create_tables.sql")
+            with open(sql_file, "r") as file:
+                sql_script = file.read()
+            conn.executescript(sql_script)
+            print("Tables created successfully.")
+    except sqlite3.Error as e:
+        print("Error creating tables:", e)
+
+def insert_data_from_csv():
+    """Function to use pandas to read data from CSV files (in 'data' folder)
+    and insert the records into their respective tables."""
+    try:
+        author_data_path = pathlib.Path("data", "authors.csv")
+        book_data_path = pathlib.Path("data", "books.csv")
+        authors_df = pd.read_csv(author_data_path)
+        books_df = pd.read_csv(book_data_path)
+        with sqlite3.connect(db_file) as conn:
+            # use the pandas DataFrame to_sql() method to insert data
+            # pass in the table name and the connection
+            authors_df.to_sql("authors", conn, if_exists="replace", index=False)
+            books_df.to_sql("books", conn, if_exists="replace", index=False)
+            print("Data inserted successfully.")
+    except (sqlite3.Error, pd.errors.EmptyDataError, FileNotFoundError) as e:
+        print("Error inserting data:", e)
+
+def execute_sql_from_file(db_filepath, sql_file):
+    with sqlite3.connect(db_filepath) as conn:
+        with open(sql_file, 'r') as file:
+            sql_script = file.read()
+        conn.executescript(sql_script)
+        print(f"Executed SQL from {sql_file}")
+db_filepath = 'project.db'
 def main():
     create_database()
+    create_tables()
+    insert_data_from_csv()
+    
+    execute_sql_from_file(db_filepath, 'sql_file/delete_records.sql')
 
 if __name__ == "__main__":
     main()
---
+++ sql_file/delete_records.sql
@@ -0,0 +1,2 @@
+DELETE FROM books WHERE title IN ('Among the Hidden', 'Brave New World', 'The Hobbit', 'The Catcher in the Rye',
+'1984');
